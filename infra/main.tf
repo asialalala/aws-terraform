18d72baac50ff8d2de6a440f4b50e3184c6cd86c
Joasia Klasik <asklas@op.pl>
2025-05-27 23:52:04 +0200

    Add sqs

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ data "aws_iam_role" "lab_role" {
   name = "LabRole"
 }
 
-# # Creates SQS queue that receives data from the distributor Lambda function.
-# resource "aws_sqs_queue" "data_queue" {
-#   name = "data-queue"
-# }
+# Creates SQS queue that receives data from the distributor Lambda function.
+resource "aws_sqs_queue" "data_queue" {
+  name = "data-queue"
+}
